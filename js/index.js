3dc98324f98a26c8526d89f24495d1cf1f1bb6dd
Monika Majer <majer_monika@interia.pl>
2018-12-05 21:14:54 +0100

    Change js code

--- js/index.js
+++ js/index.js
@@ -1,9 +1,13 @@
-$(function(){
-	var carouselList = $("#carousel ul");
-	var changeSlide = setInterval(function() {
-		$ (carouselList).animate({marginLeft:-400},1000,function() {
-			$(this).find("li:last").after($(this).find("li:first"));
-			$(this).css({marginLeft:0});
-		})
-	},3000);
+$(function () {
+  var carouselList = $("#carousel ul");
+  var changeSlide = setInterval(function () {
+    $(carouselList).animate({
+      marginLeft: -400
+    }, 1000, function () {
+      $(this).find("li:last").after($(this).find("li:first"));
+      $(this).css({
+        marginLeft: 0
+      });
+    })
+  }, 3000);
 });
